3243dfd8654c0e937ef51bf48e17b68645666b99
Léo TIOLLIER <ltiollier30@gmail.com>
2026-01-13 22:39:11 +0100

    🐛 fix email verification redirect url

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -52,6 +52,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       email,
       password,
       options: {
+        emailRedirectTo: window.location.origin,
         data: {
           display_name: displayName,
         },
